9ee7fcb5329659144184aded90271ec6b9a12ab6
afiya27taskeen-byte <afiya27taskeen@gmail.com>
2025-11-11 20:34:53 +0530

    Add files via upload

---
+++ call back function eg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void sayHello()
+{
+printf("Hello , student! you just called me back!\n");
+}
+void studentWork(void(*callbackFunc)())
+{
+printf("Student is doing homework....\n");
+callbackFunc();
+}
+int main()
+{
+studentWork(sayHello);
+return 0;
+}
---
+++ no arguement and no return value[eg 4 unit 2].c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int getNumber()
+{
+int num;
+printf("Enter a number: ");
+scanf("%d",&num);
+return num;
+}
+int main(){
+int n=getNumber();
+printf("You enterd : %D",n);
+}
