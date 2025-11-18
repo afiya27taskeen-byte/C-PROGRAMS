b3f5094fd4dd20eb5b9dd4ed64a7c5e7802b6634
afiya27taskeen-byte <afiya27taskeen@gmail.com>
2025-11-18 11:13:01 +0530

    Add files via upload

---
+++ binary search for unsorted array.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+void sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+int main() {
+    int n, key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements (unsorted):\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    sort(arr, n);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Element found at index %d (after sorting)\n", result);
+    else
+        printf("Element not found.\n");
+
+    return 0;
+}
