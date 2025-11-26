cc48914e6a01bd71611a2ab044c74b5012689948
afiya27taskeen-byte <afiya27taskeen@gmail.com>
2025-11-26 14:20:17 +0530

    Add files via upload

---
+++ typedf of an employee.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include <stdio.h>
+typedef struct
+{
+    int id;
+    char name[50];
+    float salary;
+}
+ Employee;
+int main()
+{
+    Employee emp[3];
+    int i;
+    printf("Enter details of 3 employees:\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("\nEmployee %d\n", i + 1);
+        printf("Enter ID: ");
+        scanf("%d", &emp[i].id);
+        printf("Enter Name: ");
+        scanf(" %[^\n]", emp[i].name);
+        printf("Enter Salary: ");
+        scanf("%f", &emp[i].salary);
+    }
+    printf("\n---- Employee Details ----\n");
+    for(i = 0; i < 3; i++) {
+        printf("\nEmployee %d:\n", i + 1);
+        printf("ID: %d\n", emp[i].id);
+        printf("Name: %s\n", emp[i].name);
+        printf("Salary:%.2f\n", emp[i].salary);
+    }
+    return 0;
+}
