13b8ccc031a21d4b0e9efad52adc59e2a92f45d2
afiya27taskeen-byte <afiya27taskeen@gmail.com>
2026-02-10 11:34:25 +0530

    demonstrate union memory

--- main.c
+++ main.c
@@ -1,35 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-
-// Inner structure for Department
-struct Department {
-    int cse;          // Number of students in CSE
-    int it;           // Number of students in IT
-    int aiml;         // Number of students in AIML
-    int data_science; // Number of students in Data Science
+#include<stdio.h>
+struct Date
+{
+int day;
+int mon;
+int yr;
 };
-
-// Main structure for College
-struct College {
-    char name[50];
-    char city[50];
-    struct Department dept;
+struct Student{
+int id;
+char name[20];
+struct Date dob;
 };
-int main() {
-    struct College college1;
-    strcpy(college1.name, "Garden city Engineering College");
-    strcpy(college1.city, "Bengaluru");
-    college1.dept.cse = 120;
-    college1.dept.it = 100;
-    college1.dept.aiml = 80;
-    college1.dept.data_science = 60;
-    printf("College Name: %s\n", college1.name);
-    printf("City: %s\n", college1.city);
-    printf("Departments:\n");
-    printf("  CSE: %d students\n", college1.dept.cse);
-    printf("  IT: %d students\n", college1.dept.it);
-    printf("  AIML: %d students\n", college1.dept.aiml);
-    printf("  Data Science: %d students\n", college1.dept.data_science);
-
-    return 0;
+int main(){
+struct Student s1 = {101," Kiran",{15,8,2000}};
+printf("Name: %s\n",s1.name);
+printf("DOB: %d,%d,%d\n", s1.dob.day,s1.dob.mon,s1.dob.yr);
+return 0;
 }
